d8450a48aee79f78bf21cba94a6d1761547d1838
yangshijun <yangshijunxxx@gmail.com>
2024-06-16 03:10:12 +0800

    feat: 登录模块

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
+interface SigninDto {
+  username: string;
+  password: string;
+}
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  login(@Body() signinDto: SigninDto) {
+    return this.authService.sinIn(signinDto.username, signinDto.password);
+  }
+}

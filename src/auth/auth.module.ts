9afb7d2bc90bfa55a70b9dcb13840455839a5dc4
yangshijun <yangshijunxxx@gmail.com>
2024-06-18 01:48:27 +0800

    feat: jwt鉴权

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
-// import { ConfigService } from '@nestjs/config';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('DTEST_APP_SERCET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService], // 注入ConfigService
+      imports: [ConfigModule],
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,14 @@ import { UsersService } from '@/users/users.service';
 // 自定义返回异常结果
 // import { MyCustomHttpException } from '@/common/exceptions/my-custom-http.exception';
 import { I18nContext, I18nService } from 'nestjs-i18n';
+import { JwtService } from '@nestjs/jwt';
+
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly i18n: I18nService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async sinIn(username: string, password: string) {
@@ -15,7 +18,13 @@ export class AuthService {
 
     // 这里应该进行密码验证
     if (user && user.password === password) {
-      return user;
+      // 生成token
+      const payload = { username, sub: user.id };
+      return {
+        ...user,
+        access_token: this.jwtService.sign(payload),
+      };
+      // return user;
     } else {
       // console.log(
       //   '测试',

399edfe7529b1303a015cff6b7ead13187d94c9c
yangshijun <yangshijunxxx@gmail.com>
2024-06-21 18:29:33 +0800

    feat: 更改用户servise

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,12 @@ import { UsersController } from './users.controller';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/user/user.entity';
+import { Role } from '@/entities/role/role.entity';
+import { Permission } from '@/entities/permission/permission.entity';
 
 @Module({
   // 引入注册实体
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Permission, Role])],
   providers: [UsersService, ConfigService],
   controllers: [UsersController],
   exports: [UsersService], // 导出UsersService，以便在其他模块中使用
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,8 @@ import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user/user.entity';
 import { Repository } from 'typeorm';
+import { Role } from '@/entities/role/role.entity';
+import { Permission } from '@/entities/permission/permission.entity';
 
 @Injectable()
 export class UsersService {
@@ -10,6 +12,10 @@ export class UsersService {
     private readonly configService: ConfigService,
     // 将 TypeORM 的 Repository 注入到服务类中，以便你可以在服务中使用 Repository 对象来执行数据库操作
     @InjectRepository(User) private userRepository: Repository<User>,
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>,
+    @InjectRepository(Permission)
+    private readonly permissionRepository: Repository<Permission>,
   ) {}
   findAll(): Promise<User[]> {
     return this.userRepository.find();
@@ -19,6 +25,11 @@ export class UsersService {
   }
   findOneByUsername(username: string): Promise<User> {
     return this.userRepository.findOneBy({ username });
+  }
+  // 获取用户权限
+  async getUserPermissions(user: User): Promise<Permission[]> {
+    const permissions = user.roles.flatMap((role) => role.permissions);
+    return permissions;
   }
   // 获取配置
   getConfig() {
